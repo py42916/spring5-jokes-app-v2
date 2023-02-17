9651085830120e938ccdad71bdc08a59b8be903a
Paul Young <thepaulayoung@gmail.com>
2023-02-16 18:10:24 -0800

    Initial Spring boot (maven) project creation

---
+++ src/main/java/com/example/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,15 @@
+package com.example.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		run(Spring5JokesAppV2Application.class, args);
+	}
+
+}
